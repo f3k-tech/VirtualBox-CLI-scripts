196a45e1c9dfc70adbfb25e1d5b40f009e311e39
Freek <freek@olgers.com>
2020-03-01 14:19:05 +0100

    add list vms

--- vmsettings.sh
+++ vmsettings.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# List VBoxes:
+echo "List of Virtual Machines:"
+VBoxManage list vms
+
 # Ask for name
-read -e -i "$1" -p "Enter name of the new VBox:" input
+read -e -i "$1" -p "Enter name of the VBox you'd like to edit:" input
 name=${input:-"$1"}
+
